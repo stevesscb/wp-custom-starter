f5537704dab51c5fd79dbd827b9027ba9f60b0ab
K Chan <k.chan@grayscale.com.hk>
2020-08-21 14:30:33 +0800

    Update style changes and newly supported nav feature

--- src/templates/functions-core.php
+++ src/templates/functions-core.php
@@ -60,7 +60,7 @@
     add_theme_support( 'automatic-feed-links' );
     add_theme_support( 'custom-logo' );
     add_theme_support( 'editor-styles' );
-    add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'script', 'search-form', 'style' ) );
+    add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'navigation-widgets', 'script', 'search-form', 'style' ) );
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'responsive-embeds' );
     add_theme_support( 'title-tag' );
