48025061d782c64bc9efbca14dc85e419c6a1955
K Chan <k.chan@grayscale.com.hk>
2020-12-07 11:05:15 +0800

    Added revision control and removed user endpoints from public access

--- src/templates/functions.php
+++ src/templates/functions.php
@@ -4,6 +4,21 @@ require 'functions-core.php';
 require 'functions-gutenberg.php';
 require 'functions-widgets.php';
 
+/* ACCESS CONTROL */
+  add_filter( 'rest_endpoints', function( $endpoints ) {
+    if ( ! is_user_logged_in() ) {
+      if ( isset( $endpoints['/wp/v2/users'] ) ) {
+        unset( $endpoints['/wp/v2/users'] );
+      }
+
+      if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
+        unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
+      }
+    }
+
+    return $endpoints;
+  });
+
 /* CONTENT */
   if ( ! isset( $content_width ) ) {
     $content_width = 1200;
@@ -41,3 +56,8 @@ require 'functions-widgets.php';
       echo '<style>h1::after{content:url("https://chart.googleapis.com/chart?cht=qr&chs=150x150&chl=' . get_the_permalink() . '&choe=UTF-8&chld=L|0");position:absolute;top:0;right:0}@media print{h1{padding-right:200px;text-align:left}}@media screen{h1::after{opacity:0}}</style>';
     }
   }, 20 );
+
+/* REVISIONS */
+  add_filter( 'wp_revisions_to_keep', function( $num, $post ) {
+    return 10;
+  }, 10, 2 );
